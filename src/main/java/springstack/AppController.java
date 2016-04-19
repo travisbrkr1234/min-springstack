e2067ae126cfb2b788a3407e76b2e542843d0eeb
travisbrkr1234 <carlos.ochoa@infusionsoft.com>
2016-04-19 08:13:34 -0700

    minified springboot

---
+++ src/main/java/springstack/AppController.java
@@ -0,0 +1,14 @@
+package springstack;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AppController {
+
+    @RequestMapping("/")
+    public String home() {
+        return "home";
+    }
+
+}
